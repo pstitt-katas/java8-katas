f9ce48d9bfe4f41c427902e00c85ad7407db9ae4
stittp <PaulStitt@Rentalcars.com>
2018-04-02 11:16:38 +0100

    Created examples of fizzbuzz, bracket matching and anagrams katas

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# java8-katas
+# USAGE
+
+## Build and deploy archetype to artefactory
+
+- mvn archetype:create-from-project
+- cd target/generated-sources/archetype
+- mvn versions:set -DnewVersion=20180310
+- mvn deploy
+
+## Intellij setup
+
+- Add to archetype file manually. E.g. on a mac
+- vi /Users/stittp/Library/Caches/IntelliJIdea2017.3/Maven/Indices/UserArchetypes.xml
+Add/Edit to get something like:
+```xml
+    <archetypes>
+         <archetype groupId="com.rentalcars.stittp" artifactId="java8tddArchetype-archetype" version="20170310" />
+    </archetypes>
+```
+- Create a new project, tick from archetype, then choose archetype
---
+++ src/main/java/kata/brackets/functional/ReducingChecker.java
@@ -0,0 +1,30 @@
+package kata.brackets.functional;
+
+import kata.brackets.GrammarChecker;
+import kata.brackets.Token;
+import kata.brackets.oo.ParseContext;
+
+import java.util.stream.Stream;
+
+public class ReducingChecker implements GrammarChecker {
+
+    @Override
+    public boolean isBalanced(Stream<Token> tokenStream) {
+        return tokenStream.reduce(
+                new ParseContext(),
+                (ctx, token) -> updateContext(token, ctx),
+                (t1,t2) -> t2
+        )
+        .getDepth() == 0;
+    }
+
+    private ParseContext updateContext(Token token, ParseContext context) {
+        if (token == Token.OPEN_BRACKET) {
+            context = context.startSubcontext();
+        }
+        else if (token == Token.CLOSE_BRACKET) {
+            context = context.endSubcontext();
+        }
+        return context;
+    }
+}
